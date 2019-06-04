7fb99ec59b660e86fe08b4f6c48679f0c998dc7d
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-06-04 18:05:54 +0900

    add inputs to conversion

---
+++ launch_universc.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+dr=''
+install=false
+ver_info=`paste -d "\n" <(cellranger count --version) <(echo conversion script version 0.1)`
+help="Usage: bash $(basename "$0") -R1=FILE1 -R2=FILE2 -t=TECHNOLOGY [--option=OPT]
+bash $(basename "$0") -v
+bash $(basename "$0") -h
+
+Convert sequencing data (FASTQ) from various platforms for compatibility with 10x Genomics
+
+Mandatory arguments to long options are mandatory for short options too.
+  -R1, --read1=FILE     Read 1 FASTQ file to pass to cellranger (cell barcodes and umi)
+  -R2, --read2=FILE     Read 2 FASTQ file to pass to cellranger
+  -f,  --file=NAME      Name of FASTQ files to pass to cellranger
+  -t,  --technology=PLATFORM    Name of technology used to generate data (10x, nadia, icell8)
+  -h,  --help     display this help and exit
+  -v,  --version  output version information and exit
+"
+skip=false
+
+for op in "$@";do
+    if $skip;then skip=false;continue;fi
+    case "$op" in
+        -v|--version)
+            echo "$ver_info"
+            shift
+            exit 0
+            ;;
+        -h|--help)
+            echo "$help"
+            shift
+            exit 0
+            ;;
+        -t|--technology)
+            shift
+            if [[ "$1" != "" ]]; then
+                technology="${1/%\//}"
+                shift
+                skip=true
+            else
+                echo "Error: File missing for --read1"
+                exit 1
+            fi
+            ;;
+
+        -f|--file)
+            shift
+            if [[ "$1" != "" ]]; then
+                read1="${1/%\//}_R1"
+                read2="${1/%\//}_R2"
+                shift
+                skip=true
+            else
+                echo "Error: File missing for --read1"
+                exit 1
+            fi
+            ;;
+        -R1|--read1)
+            shift
+            if [[ "$1" != "" ]]; then
+                read1="${1/%\//}"
+                shift
+                skip=false
+            else
+                echo "Error: File missing for --read1"
+                exit 1
+            fi
+            ;;
+        -R2|--read2)
+            shift
+            if [[ "$1" != "" ]]; then
+                read1="${1/%\//}"
+                shift
+                skip=true
+            else
+                echo "Error: File missing for --read1"
+                exit 1
+            fi
+            ;;
+        -*)
+            echo "Error: Invalid option: $1"
+            shift
+            exit 1
+            ;;
+    esac
+done
+
+echo technology: $technology
+echo files: $read1 (Read1) and $read2 (Read2)
