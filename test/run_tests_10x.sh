46a89dd77e0e8e8e1605bf931356f286ddac26dc
TomKellyGenetics <tomkellygenetics@gmail.com>
2020-06-23 18:03:03 +0900

    pass variables to test script

--- test/run_tests_10x.sh
+++ test/run_tests_10x.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # run tests in home directory (writeable)
-cd ~
+#cd ~
+
+cellrangerversion=`cellranger count --version | head -n 2 | tail -n 1 | cut -f2 -d'(' | cut -f1 -d')'`
+cellrangerpath=`which cellranger`
 
 # reset barcodes for test
 bash /universc/launch_universc.sh -t "10x" --setup
 
 ## test 10x data
 # unzip input data
-if [[ ! -f /cellranger-3.0.2.9001/cellranger-cs/3.0.2.9001/lib/python/cellranger/barcodes/3M-february-2018.txt.gz ]]; then
-    gzip /cellranger-3.0.2.9001/cellranger-cs/3.0.2.9001/lib/python/cellranger/barcodes/3M-february-2018.txt
+if [[ ! -f ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barcodes/3M-february-2018.txt.gz ]]; then
+    gzip ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barcodes/3M-february-2018.txt
 fi
 # test cellranger call
 cellranger testrun --id="tiny-test"
 # unzip input data
 gunzip -fk /universc/test/shared/cellranger-tiny-fastq/3.0.0/*fastq.gz
-gunzip -fk /cellranger-3.0.2.9001/cellranger-cs/3.0.2.9001/lib/python/cellranger/barcodes/3M-february-2018.txt.gz 
+gunzip -fk ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barcodes/3M-february-2018.txt.gz 
 # test cellranger call
 cellranger count --id="tiny-count-v3" \
- --fastqs="/cellranger-3.0.2.9001/cellranger-tiny-fastq/3.0.0/" --sample="tinygex" \
- --transcriptome="/cellranger-3.0.2.9001/cellranger-tiny-ref/3.0.0"
+ --fastqs="test/shared/cellranger-tiny-fastq/3.0.0/" --sample="tinygex" \
+ --transcriptome="test/cellranger_reference/cellranger-tiny-ref/3.0.0"
 
 cellranger count --id="tiny-count-v2" \
- --fastqs="/cellranger-3.0.2.9001/cellranger-tiny-fastq/1.2.0/" --sample="tinygex" \
- --transcriptome="/cellranger-3.0.2.9001/cellranger-tiny-ref/1.2.0"
+ --fastqs="test/shared/cellranger-tiny-fastq/1.2.0/" --sample="tinygex" \
+ --transcriptome="test/cellranger_reference/cellranger-tiny-ref/1.2.0"
 
 # call convert on 10x with multiple lanes
 bash /universc/launch_universc.sh --id "test-10x-v3" --technology "10x" \
