b34d3c1976dc8d1232cbc43abb066b7456d395ae
TomKellyGenetics <tomkellygenetics@gmail.com>
2020-07-03 17:51:00 +0900

    update test data for 10x

--- test/run_tests_10x.sh
+++ test/run_tests_10x.sh
@@ -3,13 +3,13 @@
 # run tests in home directory (writeable)
 #cd ~
 
-#export PATH=/home/tom/local/bin/cellranger-3.1.0:$PATH
+export PATH=/home/tom/local/bin/cellranger-3.1.0:$PATH
 
 cellrangerversion=`cellranger count --version | head -n 2 | tail -n 1 | cut -f2 -d'(' | cut -f1 -d')'`
 cellrangerpath=`which cellranger`
 
 # reset barcodes for test
-bash launch_universc.sh -t "10x" --setup
+#bash launch_universc.sh -t "10x" --setup
 
 ## test 10x data
 # unzip input data
@@ -17,18 +17,18 @@ if [[ ! -f ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barco
     gzip ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barcodes/3M-february-2018.txt
 fi
 # test cellranger call
-cellranger testrun --id="tiny-test"
+#cellranger testrun --id="tiny-test"
 # unzip input data
 gunzip -fk test/shared/cellranger-tiny-fastq/3.0.0/*fastq.gz
 gunzip -fk ${cellrangerpath}-cs/${cellrangerversion}/lib/python/cellranger/barcodes/3M-february-2018.txt.gz 
 # test cellranger call
-cellranger count --id="tiny-count-v3" \
- --fastqs="test/shared/cellranger-tiny-fastq/3.0.0/" --sample="tinygex" \
- --transcriptome="test/cellranger_reference/cellranger-tiny-ref/3.0.0"
+#cellranger count --id="tiny-count-v3" \
+# --fastqs="test/shared/cellranger-tiny-fastq/3.0.0/" --sample="tinygex" \
+# --transcriptome="test/cellranger_reference/cellranger-tiny-ref/3.0.0"
 
-cellranger count --id="tiny-count-v2" \
- --fastqs="test/shared/cellranger-tiny-fastq/1.2.0/" --sample="" --chemistry="threeprime" \
- --transcriptome="test/cellranger_reference/cellranger-tiny-ref/1.2.0"
+#cellranger count --id="tiny-count-v2" \
+# --fastqs="test/shared/cellranger-tiny-fastq/1.2.0/" --sample="" --chemistry="threeprime" \
+# --transcriptome="test/cellranger_reference/cellranger-tiny-ref/1.2.0"
 
 # call convert on 10x with multiple lanes
 bash launch_universc.sh --id "test-10x-v3" --technology "10x" \
@@ -38,8 +38,7 @@ bash launch_universc.sh --id "test-10x-v3" --technology "10x" \
 
 #bash launch_universc.sh --id "test-10x-v2" --technology "10x" \
 # --reference "test/cellranger_reference/cellranger-tiny-ref/1.2.0" \
-# --file "test/shared/cellranger-tiny-fastq/1.2.0/" \
-# "test/shared/cellranger-tiny-fastq/1.2.0/"
+# --file "test/shared/cellranger-tiny-fastq/1.2.0/"
 
 # compress all input files
-gzip -f test/shared/cellranger-tiny-fastq/3.0.0/*fastq
+#gzip -f test/shared/cellranger-tiny-fastq/3.0.0/*fastq
