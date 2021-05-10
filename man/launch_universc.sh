eb432c189acdd4f67e1e49f72493f0fb64184fb4
TomKellyGenetics <tomkellygenetics@gmail.com>
2021-05-10 17:09:17 +0900

    update documentation for non-UMI ICELL8 5' scRNA

--- man/launch_universc.sh
+++ man/launch_universc.sh
@@ -203,7 +203,7 @@ Provides a conversion script to run multiple technologies and custom libraries w
                                   Drop-Seq (12 bp barcode, 8 bp UMI): dropseq
                                   ICELL8 version 2 (11 bp barcode, No UMI): icell8-non-umi, icell8-v2
                                   ICELL8 version 3 (11 bp barcode, 14 bp UMI): icell8 or custom
-                                  ICELL8 5′ scRNA with TCR OR kit (10bp barcode, 8 bp UMI): icell8-5-prime
+                                  ICELL8 5′ scRNA with TCR OR kit (10bp barcode, NO bp UMI): icell8-5-prime
                                   ICELL8 full-length scRNA with Smart-Seq (16 bp barcode, No UMI): icell8-full-length
                                   inDrops version 1 (19 bp barcode, 6 bp UMI): indrops-v1, 1cellbio-v1
                                   inDrops version 2 (19 bp barcode, 6 bp UMI): indrops-v2, 1cellbio-v2
