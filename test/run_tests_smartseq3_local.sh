4cdc8d7afd9fbbca643c7b1c9112230d0d5fb7cc
TomKellyGenetics <tomkellygenetics@gmail.com>
2021-05-12 00:41:51 +0900

    update smartseq3 tests for TSO and dual indexes

--- test/run_tests_smartseq3_local.sh
+++ test/run_tests_smartseq3_local.sh
@@ -20,14 +20,16 @@ if [[ ! -f test/cellranger_reference/cellranger-tiny-ref/1.2.0/star/SA ]] && [[ 
 fi
 
 # compress all input files
-if [[ -f test/shared/smartseq3-test/*fastq ]]; then
+if [[ -f test/shared/smartseq3-test/Smartseq3_diySpike_S1_R1.fastq ]]; then
     gzip test/shared/smartseq3-test/*fastq
 fi
 
-mv  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_L001_R1_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_R1.fastq.gz
-mv  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_L001_R2_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_R2.fastq.gz
-mv  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_L001_I1_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_I1.fastq.gz
-mv  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_L001_I2_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike5_S1_I2.fastq.gz
+mv  test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R1_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike_S1_R1.fastq.gz
+mv  test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R2_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike_S1_R2.fastq.gz
+mv  test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_I1_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike_S1_I1.fastq.gz
+mv  test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_I2_001.fastq.gz  test/shared/smartseq3-test/Smartseq3_diySpike_S1_I2.fastq.gz
+rename -f "s/_S1_L001/_L001/" test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_[IR][12].fastq.gz
+rename -f "s/_S1//" test/shared/smartseq3-test/Smartseq3_diySpike_S1_[IR][12].fastq.gz
 
 # test manual setup
 bash launch_universc.sh -t "smartseq3" --setup
@@ -39,8 +41,8 @@ if [[ -d test-smartseq3 ]]; then
     rm -rf test-smartseq3
 fi
 
-if [[ ! -f ${whitelistdir}/SmartSeq3_test_barcodes.txt ]]; then
-    gunzip -k ${whitelistdir}/SmartSeq3_test_barcodes.txt.gz
+if [[ ! -f whitelists/SmartSeq3_test_barcodes.txt ]]; then
+    gunzip -k whitelists/SmartSeq3_test_barcodes.txt.gz
 fi
 
 # call on smartseq3 with files
@@ -48,7 +50,7 @@ bash launch_universc.sh --id "test-smartseq3" --technology "smartseq3" \
  --reference "test/cellranger_reference/cellranger-tiny-ref/3.0.0" \
  --read1 "test/shared/smartseq3-test/Smartseq3_diySpike_R1" \
  --read2 "test/shared/smartseq3-test/Smartseq3_diySpike_R2" \
- --barcodefile "${whitelistdir}/SmartSeq3_test_barcodes.txt" \
+ --barcodefile "whitelists/SmartSeq3_test_barcodes.txt" \
  --per-cell-data --jobmode "local" --localcores 1
 
 if [ -f test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R1_001.fastq.gz ]; then
