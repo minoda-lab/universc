f9ae86866a840a7b044bb5b7f0bb40cba8f7a0c2
TomKellyGenetics <tomkellygenetics@gmail.com>
2021-07-27 13:43:22 +0900

    increment development version

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
-version=1.1.3
-sed -i "s/1.1.2/1.1.3/g"  *md *html launch_universc.sh .version man/*sh inst/*
+version=1.1.4
+sed -i "s/1.1.3/1.1.4/g"  *md *html launch_universc.sh .version man/*sh inst/*
 R -e "knitr::knit('README.Rmd')"; pandoc -f markdown -t html README.md > README.html
 echo "building version $version"
 docker build -t universc:1.1.3 .
