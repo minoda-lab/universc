b43454cf3c962fdee6eb3b67dc19771f87d534ac
TomKellyGenetics <tomkellygenetics@gmail.com>
2022-01-26 11:44:12 +0900

    update documentation for 1.2.0

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
-version=1.1.7
-old_version=1.1.6.2
+version=1.2.0
+old_version=1.1.7
 sed -i "s/$old_version/$version/g"  *md *html launch_universc.sh .version man/*sh inst/*
 R -e "knitr::knit('README.Rmd')"; pandoc -f markdown -t html README.md > README.html
 echo "updating GitHub version $version"
