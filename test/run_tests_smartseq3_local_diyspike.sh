5245ab971c10ba8d4049b8b26b78d7a5d024795b
TomKellyGenetics <tomkellygenetics@gmail.com>
2022-01-26 11:40:45 +0900

    update HEK293T test data for SmartSeq3

--- test/run_tests_smartseq3_local_diyspike.sh
+++ test/run_tests_smartseq3_local_diyspike.sh
@@ -21,6 +21,17 @@ if [[ ! -f test/cellranger_reference/cellranger-tiny-ref/1.2.0/star/SA ]] && [[ 
     rsync $(dirname $cellrangerpath)/cellranger-tiny-ref/1.2.0/star/SA test/cellranger_reference/cellranger-tiny-ref/1.2.0/star/SA
 fi
 
+# compress all input files
+if [[ -f test/shared/smartseq3-test/Smartseq3.diySpike.R1.fastq ]]; then
+echo    gzip test/shared/smartseq3-test//Smartseq3.diySpike*fastq
+fi
+
+rename -f "s/_S1_L001//" test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_[IR][12]_001.fastq
+rename -f "s/_001//" test/shared/smartseq3-test/Smartseq3_diySpike_[IR][12]_001.fastq
+
+#rename "s/Smartseq3.diySpike./Smartseq3_diySpike_/g"  test/shared/smartseq3-test/Smartseq3.diySpike.[IR][12].fastq
+#rename "s/Smartseq3.diySpike./Smartseq3_diySpike_/g"  test/shared/smartseq3-test/Smartseq3.diySpike.L001.[IR][12].fastq
+
 while [[ ! -f /home/tom/repos/universc/test-10x-hek293t/outs/metrics_summary.csv  ]]; do
     echo "wait ..."
     sleep 60
@@ -29,32 +40,29 @@ done
 # test manual setup
 bash launch_universc.sh -t "smartseq3" --setup --barcodefile "whitelists/test_bcs_small.txt"
 
-if [[ -d input4cellranger_test-smartseq3-hek293t-test-chr21 ]]; then
-    rm -rf input4cellranger_test-smartseq3-hek293t-test-chr21
+if [[ -d input4cellranger_test-smartseq3-diyspike-hg38 ]]; then
+    rm -rf input4cellranger_test-smartseq3-diyspike-hg38
 fi
-if [[ -d test-smartseq3-hek293t-test-chr21 ]]; then
-    rm -rf test-smartseq3-hek293t-test-chr21
+if [[ -d test-smartseq3-diyspike-hg38 ]]; then
+    rm -rf test-smartseq3-diyspike-hg38
 fi
 
 if [[ ! -f whitelists/SmartSeq3_test_barcodes.txt ]]; then
     gunzip -k whitelists/SmartSeq3_test_barcodes.txt.gz
 fi
 
-
-#gzip -fk test/shared/smartseq3-test/Smartseq3_diySpike_[IR][12].fastq
-
  # call on smartseq3 with files
-bash launch_universc.sh --id "test-smartseq3-hek293t-test-chr21" --technology "smartseq3" \
+bash launch_universc.sh --id "test-smartseq3-diyspike-hg38" --technology "smartseq3" \
  --chemistry "SC5P-PE" \
- --reference "test/cellranger_reference/cellranger-tiny-ref/3.0.0" \
- --read1 "test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_R1_001.fastq.gz" \
- --read2 "test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_R2_001.fastq.gz" \
- --index1 "test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_I1_001.fastq.gz" \
- --index2 "test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_I2_001.fastq.gz" \
+ --reference "~/reference/cellranger/refdata-cellranger-GRCh38-3.0.0" \
+ --read1 "test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R1_001.fastq.gz" \
+ --read2 "test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R2_001.fastq" \
+ --index1 "test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_I1_001.fastq" \
+ --index2 "test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_I2_001.fastq" \
  --barcodefile "whitelists/Smartseq3_diySpike_small_bcs.txt" \
  --per-cell-data --jobmode "sge" --verbose #--as-is # "local" --localcores 1
 
-if [ -f test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_R1_001.fastq ]; then
-    rename "s/_S1_L001//" test/shared/smartseq3-test/test-smartseq-hek293t_S2_L001_[IR][12]_001.fastq*
+if [ -f test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_R1_001.fastq ]; then
+    rename "s/_S1_L001//" test/shared/smartseq3-test/Smartseq3_diySpike_S1_L001_[IR][12]_001.fastq*
     rename "s/_001//" test/shared/smartseq3-test/Smartseq3_diySpike_[IR][12]_001.fastq*
 fi
--- test/shared/smartseq3-test/prepare_files.sh
+++ test/shared/smartseq3-test/prepare_files.sh
@@ -8,31 +8,31 @@ rename "s/.diySpike./_diySpike_S1_L001_/g" *gz
 
 #create bowtie index
 bowtie2-build ../../test/cellranger_reference/cellranger-tiny-ref/3.0.0/fasta/genome.fa ../../test/cellranger_reference/cellranger-tiny-ref/3.0.0/fasta/genome
-bowtie2 -x ../../test/cellranger_reference/cellranger-tiny-ref/3.0.0/fasta/genome -U Smartseq3_diySpike_S1_L001_R2_001.001.fastq -S Smartseq3_diySpike_S1_L001_R2_001.001.sam
-samtools view -bS Smartseq3_diySpike_S1_L001_R2_001.001.sam > Smartseq3_diySpike_S1_L001_R2_001.001.bam
+bowtie2 -x ../../test/cellranger_reference/cellranger-tiny-ref/3.0.0/fasta/genome -U Smartseq3_diySpike_S1_L001_R2_001.fastq -S Smartseq3_diySpike_S1_L001_R2_001.sam
+samtools view -bS Smartseq3_diySpike_S1_L001_R2_001.sam > Smartseq3_diySpike_S1_L001_R2_001.bam
 
 # extract region of genome
-samtools sort -O BAM Smartseq3_diySpike_S1_L001_R2_001.001.bam > Smartseq3_diySpike_S1_L001_R2_001.001.sort.bam
-samtools index Smartseq3_diySpike_S1_L001_R2_001.001.sort.bam
-samtools view  Smartseq3_diySpike_S1_L001_R2_001.001.sort.bam  21:9825832-48085036 > Smartseq3_diySpike_S1_L001_R2_001.001.chr21.bam
-samtools view -O BAM  Smartseq3_diySpike_S1_L001_R2_001.001.sort.bam  21:9825832-48085036 > Smartseq3_diySpike_S1_L001_R2_001.001.chr21.sort.bam
-samtools sort -n Smartseq3_diySpike_S1_L001_R2_001.001.chr21.sort.bam -o Smartseq3_diySpike_S1_L001_R2_001.001.chr21.qsort.bam
+samtools sort -O BAM Smartseq3_diySpike_S1_L001_R2_001.bam > Smartseq3_diySpike_S1_L001_R2_001.sort.bam
+samtools index Smartseq3_diySpike_S1_L001_R2_001.sort.bam
+samtools view  Smartseq3_diySpike_S1_L001_R2_001.sort.bam  21:9825832-48085036 > Smartseq3_diySpike_S1_L001_R2_001.chr21.bam
+samtools view -O BAM  Smartseq3_diySpike_S1_L001_R2_001.sort.bam  21:9825832-48085036 > Smartseq3_diySpike_S1_L001_R2_001.chr21.sort.bam
+samtools sort -n Smartseq3_diySpike_S1_L001_R2_001.chr21.sort.bam -o Smartseq3_diySpike_S1_L001_R2_001.chr21.qsort.bam
 
 # export mapped regions to fastq
-bedtools bamtofastq -i Smartseq3_diySpike_S1_L001_R2_001.001.chr21.qsort.bam -fq Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq
-fastq_pair Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_R1_001.fastq
-fastq_pair Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_I1_001.001.fastq
-fastq_pair Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_I2_001.001.fastq
+bedtools bamtofastq -i Smartseq3_diySpike_S1_L001_R2_001.chr21.qsort.bam -fq Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq
+fastq_pair Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_R1_001.fastq
+fastq_pair Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_I1_001.fastq
+fastq_pair Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq Smartseq3_diySpike_S1_L001_I2_001.fastq
 
 # downsample to 250,000 reads per "lane"
-seqtk sample -s999 Smartseq3_diySpike_S1_L001_R1_001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_R1_001.fastq
-seqtk sample -s999 Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_R2_001.001.fastq
-seqtk sample -s999 Smartseq3_diySpike_S1_L001_I1_001.001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_I1_001.001.fastq
-seqtk sample -s999 Smartseq3_diySpike_S1_L001_I2_001.001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_I2_001.001.fastq
-seqtk sample -s100 Smartseq3_diySpike_S1_L001_R1_001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L002_R1_001.fastq
-seqtk sample -s100 Smartseq3_diySpike_S1_L001_R2_001.001.chr21_R2.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L002_R2_001.fastq
-seqtk sample -s100 Smartseq3_diySpike_S1_L001_I1_001.001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_I1_001.001.fastq
-seqtk sample -s100 Smartseq3_diySpike_S1_L001_I2_001.001.fastq.paired.fq 250000 > Smartseq3_diySpike_S1_L001_L001_I2_001.001.fastq
+seqtk sample -s999 Smartseq3_diySpike_S1_L001_R1_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L001_R1_001.fastq
+seqtk sample -s999 Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L001_R2_001.fastq
+seqtk sample -s999 Smartseq3_diySpike_S1_L001_I1_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L001_I1_001.fastq
+seqtk sample -s999 Smartseq3_diySpike_S1_L001_I2_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L001_I2_001.fastq
+seqtk sample -s100 Smartseq3_diySpike_S1_L001_R1_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L002_R1_001.fastq
+seqtk sample -s100 Smartseq3_diySpike_S1_L001_R2_001.chr21_R2.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L002_R2_001.fastq
+seqtk sample -s100 Smartseq3_diySpike_S1_L001_I1_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L002_I1_001.fastq
+seqtk sample -s100 Smartseq3_diySpike_S1_L001_I2_001.fastq.paired.fq 250000 > test-smartseq-hek293t_S2_L002_I2_001.fastq
 
 #subsample smaller files for testing
 rename "s/Smartseq3_diySpike_S1_L001_/Smartseq3_diySpike_S1_L001_/g" Smartseq3_diySpike_S1_L001_*fastq*
