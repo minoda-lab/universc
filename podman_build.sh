5d5811bba0f480ccc6ed8f9427d11c851413a1dd
TomKellyGenetics <tomkellygenetics@gmail.com>
2022-10-17 09:40:20 +0900

    update documentation for 1.2.4

--- podman_build.sh
+++ podman_build.sh
@@ -1,9 +1,10 @@
-version=1.2.3
-old_version=1.2.2
-#sed -i "s/$old_version/$version/g"  *md *html  launch_universc.sh .version man/*sh inst/*
+version=1.2.4
+old_version=1.2.3
+sed -i "s/$old_version/$version/g"  *md *html  launch_universc.sh .version man/*sh inst/* citation.cff
 R -e "knitr::knit('README.Rmd')"; pandoc -f markdown -t html README.md > README.html
 echo "updating GitHub version $version"
 git add -u
+git reset HEAD test/
 git commit -m "update documentation for $version"
 git tag $version
 git push --no-verify origin master
